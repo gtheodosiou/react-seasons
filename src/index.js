ee888e98d167f9cffb53c942e7fd653a70e41aa0
George <theodgeorge@gmail.com>
2019-01-07 21:53:54 +0000

    Add conditional state

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = {latitude: null, errorMessage: ''};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => {
+                this.setState({latitude: position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
         );
-        return <div>Latitude: </div>;
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <div> Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <div> Latitude: {this.state.latitude}</div>
+        }
+
+        return <div>Loading!</div>
     }
 }
+
 ReactDOM.render(<App/>, document.querySelector('#root'));
 
